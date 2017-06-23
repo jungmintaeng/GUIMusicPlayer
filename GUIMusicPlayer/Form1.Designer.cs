4b6d74cd631522273135d0e19557703a2dc47057
jungmintaeng <jungmin4025@gmail.com>
2017-06-23 09:10:26 +0900

    GUI Music Player

---
+++ GUIMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace GUIMusicPlayer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.playOrderButton = new System.Windows.Forms.Button();
+            this.musicList = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.duration = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderAddButton = new System.Windows.Forms.Button();
+            this.fileAddButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.currentPositionLabel = new System.Windows.Forms.Label();
+            this.maxPositionLabel = new System.Windows.Forms.Label();
+            this.musicProgressbar = new System.Windows.Forms.TrackBar();
+            this.playRepeatButton = new System.Windows.Forms.Button();
+            this.musiclistHideButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.musicProgressbar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ReadOnly = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(305, 29);
+            this.fileNameTextBox.TabIndex = 0;
+            // 
+            // prevButton
+            // 
+            this.prevButton.Location = new System.Drawing.Point(12, 113);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(97, 39);
+            this.prevButton.TabIndex = 1;
+            this.prevButton.Text = "이전 곡";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(115, 113);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(99, 39);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "재생";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(220, 113);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(97, 39);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "다음 곡";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // playOrderButton
+            // 
+            this.playOrderButton.Location = new System.Drawing.Point(12, 158);
+            this.playOrderButton.Name = "playOrderButton";
+            this.playOrderButton.Size = new System.Drawing.Size(97, 39);
+            this.playOrderButton.TabIndex = 4;
+            this.playOrderButton.Text = "순차 재생";
+            this.playOrderButton.UseVisualStyleBackColor = true;
+            this.playOrderButton.Click += new System.EventHandler(this.playOrderButton_Click);
+            // 
+            // musicList
+            // 
+            this.musicList.AllowDrop = true;
+            this.musicList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.duration});
+            this.musicList.Location = new System.Drawing.Point(12, 203);
+            this.musicList.Name = "musicList";
+            this.musicList.Size = new System.Drawing.Size(305, 359);
+            this.musicList.TabIndex = 5;
+            this.musicList.UseCompatibleStateImageBehavior = false;
+            this.musicList.View = System.Windows.Forms.View.Details;
+            this.musicList.DragDrop += new System.Windows.Forms.DragEventHandler(this.musicList_DragDrop);
+            this.musicList.DragEnter += new System.Windows.Forms.DragEventHandler(this.musicList_DragEnter);
+            this.musicList.DoubleClick += new System.EventHandler(this.musicList_DoubleClick);
+            this.musicList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.musicList_KeyDown);
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "파일명";
+            this.fileName.Width = 240;
+            // 
+            // duration
+            // 
+            this.duration.Text = "시간";
+            // 
+            // folderAddButton
+            // 
+            this.folderAddButton.Location = new System.Drawing.Point(115, 568);
+            this.folderAddButton.Name = "folderAddButton";
+            this.folderAddButton.Size = new System.Drawing.Size(99, 39);
+            this.folderAddButton.TabIndex = 7;
+            this.folderAddButton.Text = "폴더 추가";
+            this.folderAddButton.UseVisualStyleBackColor = true;
+            this.folderAddButton.Click += new System.EventHandler(this.folderAddButton_Click);
+            // 
+            // fileAddButton
+            // 
+            this.fileAddButton.Location = new System.Drawing.Point(12, 568);
+            this.fileAddButton.Name = "fileAddButton";
+            this.fileAddButton.Size = new System.Drawing.Size(97, 39);
+            this.fileAddButton.TabIndex = 8;
+            this.fileAddButton.Text = "파일 추가";
+            this.fileAddButton.UseVisualStyleBackColor = true;
+            this.fileAddButton.Click += new System.EventHandler(this.fileAddButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(220, 568);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(97, 39);
+            this.removeButton.TabIndex = 9;
+            this.removeButton.Text = "제거";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // currentPositionLabel
+            // 
+            this.currentPositionLabel.AutoSize = true;
+            this.currentPositionLabel.Location = new System.Drawing.Point(12, 47);
+            this.currentPositionLabel.Name = "currentPositionLabel";
+            this.currentPositionLabel.Size = new System.Drawing.Size(33, 12);
+            this.currentPositionLabel.TabIndex = 11;
+            this.currentPositionLabel.Text = "00:00";
+            // 
+            // maxPositionLabel
+            // 
+            this.maxPositionLabel.AutoSize = true;
+            this.maxPositionLabel.Location = new System.Drawing.Point(284, 47);
+            this.maxPositionLabel.Name = "maxPositionLabel";
+            this.maxPositionLabel.Size = new System.Drawing.Size(33, 12);
+            this.maxPositionLabel.TabIndex = 12;
+            this.maxPositionLabel.Text = "00:00";
+            // 
+            // musicProgressbar
+            // 
+            this.musicProgressbar.Location = new System.Drawing.Point(14, 62);
+            this.musicProgressbar.Name = "musicProgressbar";
+            this.musicProgressbar.Size = new System.Drawing.Size(303, 45);
+            this.musicProgressbar.TabIndex = 13;
+            this.musicProgressbar.Scroll += new System.EventHandler(this.musicProgressbar_Scroll);
+            // 
+            // playRepeatButton
+            // 
+            this.playRepeatButton.Location = new System.Drawing.Point(115, 158);
+            this.playRepeatButton.Name = "playRepeatButton";
+            this.playRepeatButton.Size = new System.Drawing.Size(99, 39);
+            this.playRepeatButton.TabIndex = 14;
+            this.playRepeatButton.Text = "반복 없음";
+            this.playRepeatButton.UseVisualStyleBackColor = true;
+            this.playRepeatButton.Click += new System.EventHandler(this.playRepeatButton_Click);
+            // 
+            // musiclistHideButton
+            // 
+            this.musiclistHideButton.Location = new System.Drawing.Point(220, 158);
+            this.musiclistHideButton.Name = "musiclistHideButton";
+            this.musiclistHideButton.Size = new System.Drawing.Size(97, 39);
+            this.musiclistHideButton.TabIndex = 4;
+            this.musiclistHideButton.Text = "재생목록 닫기";
+            this.musiclistHideButton.UseVisualStyleBackColor = true;
+            this.musiclistHideButton.Click += new System.EventHandler(this.musiclistHideButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 614);
+            this.Controls.Add(this.playRepeatButton);
+            this.Controls.Add(this.musicProgressbar);
+            this.Controls.Add(this.maxPositionLabel);
+            this.Controls.Add(this.currentPositionLabel);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.fileAddButton);
+            this.Controls.Add(this.folderAddButton);
+            this.Controls.Add(this.musicList);
+            this.Controls.Add(this.musiclistHideButton);
+            this.Controls.Add(this.playOrderButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.fileNameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Music Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.musicProgressbar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button playOrderButton;
+        private System.Windows.Forms.ListView musicList;
+        private System.Windows.Forms.Button folderAddButton;
+        private System.Windows.Forms.Button fileAddButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader duration;
+        private System.Windows.Forms.Label currentPositionLabel;
+        private System.Windows.Forms.Label maxPositionLabel;
+        private System.Windows.Forms.TrackBar musicProgressbar;
+        private System.Windows.Forms.Button playRepeatButton;
+        private System.Windows.Forms.Button musiclistHideButton;
+    }
+}
+
---
+++ GUIMusicPlayer/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace GUIMusicPlayer
+{
+    public partial class mainForm : Form
+    {
+        private WindowsMediaPlayer playerInstance;
+        private IWMPPlaylist playList;
+        private List<IWMPMedia> playListManager;
+        private Thread progressThread;
+        private bool isPlaying = false;
+        private IWMPMedia latestMedia;
+        private Boolean ended = false;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        //폼이 로드될 때
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            playerInstance = new WindowsMediaPlayer();
+            playList = playerInstance.newPlaylist("MusicPlayer", "");
+            playerInstance.currentPlaylist = playList;
+            playListManager = new List<IWMPMedia>();
+            progressThread = new Thread(new ThreadStart(Progress));
+            progressThread.Start();
+            playerInstance.PlayStateChange += PlayerInstance_PlayStateChange;
+        }
+
+        /*
+         * filePath를 통해 해당 Path에 존재하는 파일을 재생목록에 추가하는 메소드
+         */
+        private void addFile(string filePath)
+        {
+            if (!Path.GetExtension(filePath).Equals(".mp3"))
+            {
+                MessageBox.Show(Path.GetFileName(filePath) + "는 mp3 파일이 아닙니다.");
+                return;
+            }
+
+            //WMP 재생목록에 추가
+            IWMPMedia newMedia = playerInstance.newMedia(filePath);
+            playList.appendItem(newMedia);
+
+            //미디어 리스트에 추가
+            playListManager.Add(newMedia);
+
+            //ListView에 추가
+            ListViewItem newItem = new ListViewItem(Path.GetFileName(filePath));
+            newItem.SubItems.Add(parseToMinuteString(newMedia.duration));
+            musicList.Items.Add(newItem);
+        }
+
+        /*
+         * 분이나 초가 한 자리수이면 앞에 0을 붙여주는 메소드
+         */
+        private string leadingZero(string number)
+        {
+            if (number.Length < 2)
+            {
+                number = "0" + number;
+            }
+            return number;
+        }
+
+        /*
+         * double 형의 노래 지속 시간을 분 형태로 바꿔서 string으로 반환하는 메소드
+         */
+        private string parseToMinuteString(double duration)
+        {
+            string min = ((int)duration / 60).ToString();
+            string sec = ((int)duration % 60).ToString();
+            return leadingZero(min) + ":" + leadingZero(sec);
+        }
+
+        /*
+         * 현재 재생 중인 곡을 삭제 하는 등의 행동을 했을 때, 재생 버튼의 텍스트나
+         * 재생 중인 파일 명, ProgressBar value 등을 초기로 돌림
+         */
+        private void initState()
+        {
+            try
+            {
+                isPlaying = false;
+                fileNameTextBox.Text = "";
+                playButton.Text = "재생";
+                musicProgressbar.Value = 0;
+                musicProgressbar.Maximum = 0;
+                maxPositionLabel.Text = "00:00";
+                currentPositionLabel.Text = "00:00";
+            }
+            catch (ObjectDisposedException) { }
+        }
+
+        /*
+         * ProgressBar가 실행할 메소드
+         */
+        private void Progress()
+        {
+            while (true)
+            {
+                try
+                {
+                    Invoke(new MethodInvoker(() =>
+                {
+                    try
+                    {
+                        currentPositionLabel.Text = parseToMinuteString(playerInstance.controls.currentPosition);
+                        maxPositionLabel.Text = parseToMinuteString(playerInstance.currentMedia.duration);
+                        musicProgressbar.Maximum = (int)playerInstance.currentMedia.duration;
+                        musicProgressbar.Value = (int)playerInstance.controls.currentPosition;
+                        fileNameTextBox.Text = playerInstance.currentMedia.name;
+                    }
+                    catch (Exception)
+                    {
+                        Invoke(new MethodInvoker(() =>
+                        {
+                            initState();
+                        }));
+                    }
+                }));
+                    Thread.Sleep(500);
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        //재생 버튼 클릭 이벤트
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if(playList.count < 1)
+            {
+                MessageBox.Show("재생 목록에 곡을 추가해주세요.");
+                return;
+            }
+            if (isPlaying)
+            {
+                playerInstance.controls.pause();
+                playButton.Text = "재생";
+                isPlaying = false;
+            }
+            else
+            {
+                ended = false;
+                playerInstance.controls.play();
+                playButton.Text = "일시 정지";
+                isPlaying = true;
+            }
+        }
+
+        //이전 곡 버튼 클릭 이벤트
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            if(playerInstance.controls.currentPosition > 5)
+            {
+                playerInstance.controls.currentPosition = 0;
+            }
+            else
+            {
+                ended = false;
+                playerInstance.controls.previous();
+                latestMedia = playerInstance.currentMedia;
+            }
+        }
+
+        //다음 곡 버튼 클릭 이벤트
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            ended = false;
+            playerInstance.controls.next();
+            latestMedia = playerInstance.currentMedia;
+        }
+
+        //파일 추가 버튼 클릭 이벤트
+        private void fileAddButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "MP3 File(*.mp3)|*.mp3";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                addFile(dialog.FileName);
+            }
+        }
+
+        //폴더 추가 버튼 클릭 이벤트
+        private void folderAddButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo selectedPathInfo = new DirectoryInfo(dialog.SelectedPath);
+                foreach(var file in selectedPathInfo.GetFiles())
+                {
+                    if (!file.Extension.Equals(".mp3"))
+                    {
+                        continue;
+                    }
+                    addFile(file.FullName);
+                }
+            }
+        }
+
+        //재생 목록에서 제거 버튼 클릭 이벤트
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            foreach(var music in musicList.SelectedItems)
+            {
+                int removeIndex = musicList.Items.IndexOf((ListViewItem)music);
+                if (playerInstance.currentMedia.sourceURL.Equals(playListManager[removeIndex].sourceURL))
+                    initState();
+                musicList.Items.RemoveAt(removeIndex);
+                playList.removeItem(playListManager[removeIndex]);
+                playListManager.RemoveAt(removeIndex);
+            }
+        }
+
+        //트랙바 스크롤 시
+        private void musicProgressbar_Scroll(object sender, EventArgs e)
+        {
+            try
+            {
+                playerInstance.controls.currentPosition = musicProgressbar.Value;
+            }
+            catch (Exception) { }
+        }
+        
+        //폼 꺼질 때(리소스 해제)
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                playerInstance.close();
+            }
+            catch (Exception) { }
+            try
+            {
+                progressThread.Abort();
+            }
+            catch (Exception) { }
+        }
+
+        //재생목록 열기 닫기 버튼 클릭 이벤트
+        private void musiclistHideButton_Click(object sender, EventArgs e)
+        {
+            if (musiclistHideButton.Text.Equals("재생목록 열기"))
+            {
+                musiclistHideButton.Text = "재생목록 닫기";
+                this.Size = new Size(345, 653);
+            }
+            else if (musiclistHideButton.Text.Equals("재생목록 닫기"))
+            {
+                musiclistHideButton.Text = "재생목록 열기";
+                this.Size = new Size(345, 241);
+            }
+        }
+
+        //재생 순서 버튼 클릭 이벤트
+        private void playOrderButton_Click(object sender, EventArgs e)
+        {
+            if (playOrderButton.Text.Equals("순차 재생"))
+            {
+                playOrderButton.Text = "랜덤 재생";
+                playerInstance.settings.setMode("shuffle", true);
+            }
+            else if (playOrderButton.Text.Equals("랜덤 재생"))
+            {
+                playOrderButton.Text = "순차 재생";
+                playerInstance.settings.setMode("shuffle", false);
+            }
+        }
+        
+        //재생 반복 버튼 클릭 이벤트
+        private void playRepeatButton_Click(object sender, EventArgs e)
+        {
+            if (playRepeatButton.Text.Equals("전곡 반복"))
+            {
+                playRepeatButton.Text = "반복 없음";
+                playerInstance.settings.setMode("loop", false);
+            }
+            else if (playRepeatButton.Text.Equals("반복 없음"))
+            {
+                playRepeatButton.Text = "한곡 반복";
+                playerInstance.settings.setMode("loop", true);
+                ended = false;
+                latestMedia = playerInstance.currentMedia;
+            }
+            else if (playRepeatButton.Text.Equals("한곡 반복"))
+            {
+                playRepeatButton.Text = "전곡 반복";
+            }
+        }
+
+        //뮤직 플레이어 상태 바뀌었을 때 --> 한곡반복, 전체 반복 등 처리
+        private void PlayerInstance_PlayStateChange(int NewState)
+        {
+            if (playRepeatButton.Text.Equals("한곡 반복") && NewState == 3)
+            {
+                if (ended)
+                {
+                    ended = false;
+                    playerInstance.controls.playItem(latestMedia);
+                }
+                else
+                    ended = true;
+            }
+        }
+
+        // 파일&폴더 드래그앤드랍
+        private void musicList_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void musicList_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string filePath in files)
+            {
+                if (Path.GetExtension(filePath).Equals(".mp3"))
+                    addFile(filePath);
+
+                DirectoryInfo dir = new DirectoryInfo(filePath);
+                if (dir.Attributes.ToString().Equals("Directory"))
+                {
+                    foreach (var file in dir.GetFiles())
+                    {
+                        if (!file.Extension.Equals(".mp3"))
+                        {
+                            continue;
+                        }
+                        addFile(file.FullName);
+                    }
+                }
+            }
+        }
+
+        // 키보드 delete 버튼으로 음악 삭제 추가
+        private void musicList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+                removeButton.PerformClick();
+        }
+
+        // 마우스 더블클릭으로 음악 재생 추가
+        private void musicList_DoubleClick(object sender, EventArgs e)
+        {
+            int idx = musicList.Items.IndexOf((ListViewItem)musicList.SelectedItems[0]);
+            while (true)
+            {
+                try
+                {
+                    playerInstance.controls.playItem(playListManager[idx]);
+                    break;
+                }
+                catch (Exception)
+                {
+                    playerInstance.controls.next();
+                }
+            }
+        }
+    }
+}
